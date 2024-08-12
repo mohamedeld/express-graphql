4fd0c282424d377dfe218dd226aaf2eece875025
mohamedeld <mohamed.azoz20010@gmail.com>
2024-08-12 22:19:53 +0300

    add token to login user

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {buildSchema} = require("graphql");
 const {graphqlHTTP} = require("express-graphql");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("./Models/User");
 require("dotenv/config");
 require("./connection");
@@ -22,8 +24,13 @@ const schema = buildSchema(`
     email:String!
     password:String!
   }
+  input userLogin{
+    email:String!
+    password:String!
+  }
   type Mutation{
     createUser(input:userInput):User
+    loginUser(input:userLogin):String
   }
 `)
 const UserQueries = {
@@ -35,8 +42,22 @@ const UserQueries = {
 const UserMutations = {
   createUser:async({input})=>{
     const {name,email,password} = input;
-    const newUser = await User.create({name,email,password});
+    const hashedPassword = await bcrypt.hash(password,10);
+    const newUser = await User.create({name,email,password:hashedPassword});
     return {name,email}
+  },
+  loginUser:async({input})=>{
+    const {email,password} = input;
+    const user = await User.findOne({email});
+    if(!user){
+      throw new Error("email not found");
+    }
+    const hashPassword = await bcrypt.compare(password,user.password);
+    if(!hashPassword){
+      throw new Error("invalid email or password");
+    }
+    const token = jwt.sign({userId:user?._id},process.env.SECRET_KEY,{expiresIn:process.env.EXPIRES_AT});
+    return token
   }
 }
 const resolvers = {
